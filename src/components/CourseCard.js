fa8f0211ea99eb77cfe2c4a6318fe8d6bc696b85
danwristband <dan.akkaraphan@gmail.com>
2021-01-11 23:52:49 +0700

    first

--- src/components/CourseCard.js
+++ src/components/CourseCard.js
@@ -1,9 +1,28 @@
-const CourseCard = (props) => {
+// const CourseCard = (props) => {
+//   return (
+//     <>
+//       <p>This is Course card</p>
+//     </>
+//   );
+// };
+
+// export default CourseCard;
+
+export const CourseCard = (props) => {
+  // TODO: design HTML component that displays a course as a "card" on the webpage.
+ 
+
   return (
-    <>
-      <p>This is Course card</p>
-    </>
+
+    
+  <div className="subject">
+      <h1 className="NSubject">{props.title}
+       </h1>
+    <div className="close" onClick={()=>props.setMyCourse(props.courseNO)}>x</div>
+    <ul className="dataSubject">
+      <li>CREDIT : {props.credit}</li>
+      <li>GRADE : {props.grade}</li>
+      </ul>
+    </div>
   );
 };
-
-export default CourseCard;
